96fa21f89ea5c360fa9904973fb75d4057e36473
mionaga <143914849+mionaga@users.noreply.github.com>
2023-09-23 14:56:03 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,9 @@ class BooksController < ApplicationController
        @books= Book.all
        render :index
      end
+     @user = User.find(@book.user_id)
   end
-  
+
   def show
     @book_new=Book.new
     @book=Book.find(params[:id])
@@ -25,27 +26,27 @@ class BooksController < ApplicationController
     @books=Book.all
     @user=current_user
     @book_new=Book.new
-    
+
   end
-  
+
   def update
     @book=Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book.id)
-  end  
-  
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
-  
+
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  
+
+
 end
